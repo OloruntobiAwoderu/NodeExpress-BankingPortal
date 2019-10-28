81f5161527100f846acde505067bd92fcbfe214f
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-28 15:15:11 +0100

    reading from user data

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 const accountData = fs.readFileSync('src/json/accounts.json', { encoding: "UTF8"})
 const accounts = JSON.parse(accountData)
 
+const userData = fs.readFileSync("src/json/users.json", { encoding: 'UTF8'});
+const users = JSON.parse(userData);
+
 app.get("/" , (req,res) => {
     res.render("index", { title: "Index"})
 })
