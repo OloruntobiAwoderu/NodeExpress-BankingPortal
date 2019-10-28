5234c788e16b3209837d7519b988dda384dec523
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-28 19:09:29 +0100

    Started third module

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true}))
 app.use(express.static(path.join(__dirname, "public")));
 
 const accountData = fs.readFileSync("src/json/accounts.json", {
@@ -34,6 +35,17 @@ app.get('/checking', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.render("profile", {user: users[0]})
+})
+app.get('/transfer', (req, res) => {
+  res.render("transfer")
+})
+
+app.post('/transfer' ,(req, res) => {
+accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+const accountsJSON = JSON.stringify(accounts, null, 4);
+fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8')
+res.render('transfer', {message: "Transfer completed"});
 })
 
 
