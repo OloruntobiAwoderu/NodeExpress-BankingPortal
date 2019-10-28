713454e7a279b7f8771cad177d56687b71964a93
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-28 19:11:26 +0100

    Made change

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amo
 accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
 const accountsJSON = JSON.stringify(accounts, null, 4);
 fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8')
-res.render('transfer', {message: "Transfer completed"});
+res.render('transfer', {message: "Transfer Completed"});
 })
 
 
