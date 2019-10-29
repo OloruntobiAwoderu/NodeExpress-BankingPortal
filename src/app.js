9a6e081558100e804ca62dea09f5fb06c25c7ffc
oloruntobi <awoderuoloruntobi@gmail.com>
2019-10-29 15:59:09 +0100

    Installed more dvdependencies

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const { accounts, users, writeJSON } = require("./data");
- const accountRoutes = require("./routes/accounts");
+const accountRoutes = require("./routes/accounts");
 const serviceRoutes = require("./routes/services");
 
 app.set("views", path.join(__dirname, "views"));
